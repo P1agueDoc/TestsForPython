7785ab472bd9e0d43ae4ffa27c077d31eccf7e5d
PlagiDocy <46785308+P1agueDoc@users.noreply.github.com>
2024-08-13 16:55:37 +0500

    "Переменные"
    2023/09/22 00:00|Практическое задание по теме "Переменные"

---
+++ test3.py
@@ -0,0 +1,5 @@
+Finished_Homework = 12
+Hours_spent = 1.5
+Course_name = "Python"
+Time_on_one_Task = Hours_spent/Finished_Homework
+print("Курс:",Course_name,"всего задач:",Finished_Homework,"затрачено часов:",Hours_spent, "среднее время выполнения:",Time_on_one_Task,"часа.")
