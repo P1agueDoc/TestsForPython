10f1b4f1072ed653dd117d7668e625bbef24ff87
PlagiDocy <46785308+P1agueDoc@users.noreply.github.com>
2025-01-16 19:25:30 +0500

    Дополнительное практическое задание по модулю
    2023/10/16 00:00|Дополнительное практическое задание по модулю*

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,32 +1,22 @@
-def calculate_structure_sum(data_structure):
-    data_structure = str(data_structure)
-    counted = 0
-    index_num = -1
-    skip = 0
-    for i in data_structure:
-        index_num += 1
-        if skip > 0:
-            skip -= 1
-            continue
-        if i.isalpha():
-            counted += 1
-        elif i.isnumeric():
-            if data_structure[index_num+1].isnumeric():
-                counted += int(str(i) + str(data_structure[index_num+1]))
-                skip += 1
-            elif data_structure[index_num-1].isalpha():
-                counted += 1
-            else:
-                counted += int(i)
-    return counted
-
-
+def calculate_structure_sum(data):
+    if isinstance(data, int):
+        return data
+    elif isinstance(data, str):
+        return len(data)
+    elif isinstance(data, (list, tuple, set)):
+        return sum(calculate_structure_sum(item) for item in data)
+    elif isinstance(data, dict):
+        return sum(calculate_structure_sum(key) + calculate_structure_sum(value) for key, value in data.items())
+    else:
+        return 0
 
 data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-print(calculate_structure_sum(data_structure))
+
+result = calculate_structure_sum(data_structure)
+print(result)
